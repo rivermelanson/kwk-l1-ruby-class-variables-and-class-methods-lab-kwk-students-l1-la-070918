b2c5aa08a5f47b9ce3f1563668a90779cdb9c858
rivermelanson <mulberryandtoby@gmail.com>
2018-07-13 22:24:04 +0000

    automatically backed up by learn

--- song.rb
+++ song.rb
@@ -2,7 +2,7 @@ class Songs
   attr_reader :name, :artist, :album_name, :genre
   attr_writer :name, :artist, :album_name, :genre 
   
-  def rock_songs = Songs.new("name", "artist", "album_name", "genre")
+  def initialize(name, artist, album_name, genre)
     @name = name 
     @artist = artist 
     @album_name = album_name
@@ -10,3 +10,9 @@ class Songs
     
   end 
 end 
+
+piano_man = Songs.new("pianoman", "billy joel", "the essential billy joel", "rock and roll")
+all_star = Songs.new("all star", "smash mouth", "astro lounge", "rock/punk")
+radio_ga_ga = Songs.new("radio ga ga", "queen", "the works", "rock")
+come_and_get_your_love = Songs.new("come and get your love", "redbone", "the essential redbone", "rock/jazz")
+
