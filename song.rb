75926f1341c1970073ed125b6ac9929201af9d93
rivermelanson <mulberryandtoby@gmail.com>
2018-07-13 22:12:51 +0000

    automatically backed up by learn

--- song.rb
+++ song.rb
@@ -3,7 +3,6 @@ class Songs
   attr_writer :name, :artist, :album_name, :genre 
   
   def rock_songs = Songs.new("name", "artist", "album_name", "genre")
-    puts "who sings your song?"
     @name 
     @artist
     @album_name
